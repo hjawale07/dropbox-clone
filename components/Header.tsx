06a9ce27e4f9588009aa4634ace3cab0a860ee6a
hjawale07 <hjawale@tulip-tech.com>
2024-01-09 20:09:20 +0530

    chore: logo update
    chore: logo update

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,7 +9,7 @@ function Header() {
       <Link href="/" className="flex items-center space-x-2">
         <div className="bg-[#0160fe] w-fit">
             <Image
-                src='https:///www.shareicon.net/download/2016/07/13/606936_dropbox_2048x2048.png'
+                src='/dropbox.png'
                 alt={'logo'}
                 className="invert"
                 height={50}
